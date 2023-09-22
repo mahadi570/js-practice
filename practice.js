2b8e7a7c445ba40b44b4b110fab7bbd8d1c844db
mahadi570 <mahadihasan18407@gmail.com>
2023-09-22 13:45:10 +0400

    problems solved

---
+++ practice.js
@@ -0,0 +1,50 @@
+// 1st Problem Solution
+
+
+var amount = 1000;
+var apple = 400;
+var orange = 300;
+
+var invoice = apple + orange;
+
+var remainAmount = amount - invoice;
+console.log("Remaing Amount is " + remainAmount);
+
+
+
+// 2nd Problem Solution 
+
+
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var total = math + biology + chemistry + physics + bangla;
+
+var average = total / 5;
+average = average.toFixed(2);
+
+console.log("Average Number is " + average);
+
+
+// 3rd Problem Solution
+
+
+var firstSentence = "I am going to be";
+var lastSentence = "an awesome web developer.";
+
+var fullSentence = firstSentence + " " + lastSentence;
+console.log(fullSentence);
+
+
+// 4th Problem Solution
+
+
+var devidend = 119;
+var devisor = 5;
+
+var quotient = devidend / devisor;
+var remainder = devidend % devisor;
+console.log(remainder);
