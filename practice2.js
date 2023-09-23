e97dc81cdf0d1a324b397776cd3e7be5b2f03655
mahadi570 <mahadihasan18407@gmail.com>
2023-09-23 15:53:45 +0400

    some module problems soved

---
+++ practice2.js
@@ -0,0 +1,40 @@
+// 1st Problem Solutin \\
+
+for (let i = 1; i < 40; i++) {
+    console.log(i + ' Ajke amar mon valo nai');
+}
+
+// 2nd Problem Solution \\
+for (var i = 58; i <=98; i++ )
+{
+    console.log(i);
+}
+
+// 3rd Problem Solution \\
+for (var i = 412; i <= 456; i = i + 2){
+    console.log(i)
+}
+
+// 4th problem solution \\
+for (var i = 581; i <= 623; i = i + 2){
+    console.log(i)
+}
+
+// 5th problem solution \\
+
+// For loop is used to iterate over a sequence of items. While loop is used to repeatedly execute a block of statements while a condition is true.
+
+
+// 6th problem solution \\
+var iLearned = [ 'html', 'css', 'tailwind', 'daisyUI', 'java script'];
+console.log(iLearned);
+
+// 7th problem solution \\
+
+var myOldMobiles = ['nokia', 'samsung', 'itel', 'iphone'];
+var phone = 0;
+while( phone < myOldMobiles.length){
+    console.log(myOldMobiles[phone]);
+    
+    phone++;
+}
