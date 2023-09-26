d5e9c7332e51d5db409347fbafa65f6d8a4b6c0c
mahadi570 <mahadihasan18407@gmail.com>
2023-09-26 18:31:03 +0400

    practice7 solved

--- practice4.js
+++ practice4.js
@@ -1,9 +1,9 @@
 function make_avg(num1, num2, num3) {
 var total = num1 + num2 + num3;
 var avg = total / 3;
-avg.toFixed(2);
-return avg;
+var fixedDigit = avg.toFixed(2);
+return fixedDigit;
 }
 
-var avg1 = make_avg(30, 40, 20);
+var avg1 = make_avg(30, 20, 25);
 console.log(avg1);
